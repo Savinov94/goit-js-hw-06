d24ff986742373958fbad9e382c6b2933229e73b
Savinov94 <134193164+Savinov94@users.noreply.github.com>
2023-09-24 20:20:36 +0200

    tghd

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,10 @@
-const title = document.querySelector("h2");
-console.log(title)
+const ul = document.querySelector(`#categories`);
+const li = ul.querySelectorAll(`.item`);
+console.log(`Number of categories: ${li.length}`)
+
+li.forEach((li) => {
+    const title = li.querySelector(`h2`);
+    const subtitle = li.querySelectorAll(`ul li`);
+    console.log(`Category : ${title.textContent}`)
+    console.log(`Element: ${subtitle.length}`)
+});
