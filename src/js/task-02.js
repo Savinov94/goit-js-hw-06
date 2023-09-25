9504e4f649ff919de4f9e7863e7c88de460e21af
Savinov94 <134193164+Savinov94@users.noreply.github.com>
2023-09-25 14:08:30 +0200

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector(`#ingredients`);
+
+const liItem = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  ul.appendChild(liEl);
+  console.log(liEl);
+});
